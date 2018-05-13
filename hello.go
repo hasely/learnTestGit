6ea7b5603a68693e99d308dd574d8080acfcba0a
HuiweiDong <huiwei.dong@accenture.com>
2018-05-13 15:41:12 +0200

    test

--- hello.go
+++ hello.go
@@ -2,4 +2,6 @@ package main
 import "fmt"
 func main() {
 	fmt.Println("hello world in the branch feature-1")
+	fmt.Println("master branch has been updated, so i should do pull first to update this feature branch and then push my new code in feature branch")
+
 }
